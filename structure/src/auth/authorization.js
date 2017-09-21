68352a270fde4f205c8febafb2340733723ab421
Victor Kurauchi <victorkurauchi@gmail.com>
2017-09-21 01:44:47 -0300

    added json webtoken

--- structure/src/auth/authorization.js
+++ structure/src/auth/authorization.js
@@ -2,21 +2,30 @@ const jwt = require('jsonwebtoken')
 
 const SECRET_ADMIN = 'yoursecretadmin'
 
-module.exports.adminMiddleware = (req, res, next) => {
-  var token = req.body.token || req.query.token || req.headers['x-access-token']
-  if (token) {
-    jwt.verify(token, SECRET_ADMIN, (err, decoded) => {
-      if (err) {
-        return res.json({ success: false, message: 'Failed to authenticate token.' })
-      } else {
-        req.decoded = decoded
-        next()
-      }
-    })
-  } else {
+module.exports.verifyJWT = (req, res, next) => {
+  const token = req.body.token || req.query.token || req.headers['x-access-token']
+  if (!token) {
     return res.status(403).send({
       success: false,
       message: 'No token provided.'
     })
   }
+
+  try {
+    const decoded = jwt.verify(token, 'wrong-secret')
+    req.decoded = decoded
+    next()
+  } catch (err) {
+    console.log(err)
+    return res.json({ success: false, message: 'Failed to authenticate token.' })
+  }
+}
+
+module.exports.sign = (payload) => {
+  const generatedToken = jwt.sign(payload, SECRET_ADMIN, {
+    expiresIn: 60
+    // expiresIn: '7d'
+  })
+
+  return generatedToken
 }
